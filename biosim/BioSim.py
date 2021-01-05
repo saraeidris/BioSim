cf10bf0203a060c25bf64aeaf3a121707c02f633
saraidris <said@nmbu.no>
2021-01-05 22:38:56 +0100

    updated animal class and biosim

--- biosim/BioSim.py
+++ biosim/BioSim.py
@@ -1,3 +1,5 @@
+import random
+
 from biosim.Landscape import Highland
 from biosim.animals import Herbs
 
@@ -5,7 +7,7 @@ from biosim.animals import Herbs
 class BioSim:
     def __init__(self, island_map, ini_pop, seed,
                  ymax_animals=None, cmax_animals=None, hist_specs=None,
-                 img_base=None, img_fmt=’png’):
+                 img_base=None, img_fmt='png'):
         """
         :param island_map: Multi-line string specifying island geography
         :param ini_pop: List of dictionaries specifying initial population
@@ -22,39 +24,44 @@ class BioSim:
         If img_base is None, no figures are written to file. Filenames are formed as
             ’{}_{:05d}.{}’.format(img_base, img_no, img_fmt)
         where img_no are consecutive image numbers starting from 0. img_base should contain a path and beginning of a file name. """
-
         self.island_map = island_map
-        self.ini_pop = self.create_population(ini_pop)
         self.seed = seed
+        random.seed(seed)
         self.ymax_animals = ymax_animals
         self.cmax_animals = cmax_animals
         self.hist_specs = hist_specs
         self.img_base = img_base
-        self.img_fmt =img_fmt
-        self.carnivore_params =  {'w_birth': 6.0, 'sigma_birth': 1.0, 'beta': 0.75, 'eta': 0.125,
-                         'a_half': 40.0, 'phi_age': 0.3, 'w_half': 4.0,
-                         'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8, 'zeta': 3.5, 'xi': 1.1,
-                         'omega': 0.8, 'F': 50.0, 'DeltaPhiMax': 10.0}
+        self.img_fmt = img_fmt
+        self.carnivore_params = {'w_birth': 6.0, 'sigma_birth': 1.0, 'beta': 0.75, 'eta': 0.125,
+                                 'a_half': 40.0, 'phi_age': 0.3, 'w_half': 4.0,
+                                 'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8, 'zeta': 3.5, 'xi': 1.1,
+                                 'omega': 0.8, 'F': 50.0, 'DeltaPhiMax': 10.0}
         self.herbivore_params = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9, 'eta': 0.05,
-                         'a_half': 40.0, 'phi_age': 0.6, 'w_half': 10.0,
-                         'phi_weight': 0.1, 'mu': 0.25, 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
-                         'omega': 0.4, 'F': 10.0}
+                                 'a_half': 40.0, 'phi_age': 0.6, 'w_half': 10.0,
+                                 'phi_weight': 0.1, 'mu': 0.25, 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                                 'omega': 0.4, 'F': 10.0}
         self.lowland_params = {'f_max': 800}
         self.highland_params = {'f_max': 300}
         self.island = Highland(None, None, None, None, self.highland_params['f_max'])
+        self.ini_pop = self.create_population(ini_pop)
 
     def create_population(self, init_pop):
         animals = []
         for cell in init_pop:
-            for animal in cell:
+            for animal in cell['pop']:
                 if animal['species'] == 'Herbivore':
                     animals.append(Herbs(animal['age'],
                                          animal['weight'],
+                                         # Må fikses på når hele øya er på plass
+                                         self.island,
                                          self.herbivore_params['sigma_birth'],
                                          self.herbivore_params['phi_age'],
                                          self.herbivore_params['a_half'],
                                          self.herbivore_params['phi_weight'],
-                                         self.herbivore_params['w_half']))
+                                         self.herbivore_params['w_half'],
+                                         self.herbivore_params['beta'],
+                                         self.herbivore_params['F']))
+        print(animals)
         return animals
 
     def set_animal_parameters(self, species, params):
@@ -70,13 +77,11 @@ class BioSim:
         else:
             raise ValueError
 
-    #Hjelpemetode bør være privat
+    # Hjelpemetode bør være privat
     def merge_params(self, params, params2):
-       return {**params, **params2}
+        return {**params, **params2}
 
-
-
-    #Må fikses
+    # Må fikses
     def set_landscape_parameters(self, landscape, params):
         """
         Set parameters for landscape type.
@@ -84,10 +89,10 @@ class BioSim:
         :param params: Dict with valid parameter specification for landscape """
         if landscape == 'L':
             self.lowland_params = self.merge_params(self.lowland_params, params)
-        if landscape == 'H':
+        elif landscape == 'H':
             self.highland_params = self.merge_params(self.highland_params, params)
-        raise ValueError
-
+        else:
+            raise ValueError
 
     def simulate(self, num_years, vis_years=1, img_years=None):
         """
@@ -98,30 +103,54 @@ class BioSim:
         Image files will be numbered consecutively.
         """
 
-    def feeding(self, species):
-        isinstance()
+        for _ in range(num_years):
+            self.feeding()
+            self.grow()
+            self.age()
+            for animal in self.ini_pop:
+                print(animal.get_weight())
+                print(animal.get_fitness())
+                print(animal.get_age())
 
+    def feeding(self):
+        herbs = list(filter(lambda obj: isinstance(obj, Herbs), self.ini_pop))
+        new_herbs_list = []
+        # carnivores = list(filter(lambda obj: isinstance(obj, Carnivores), self.ini_pop))
+        while len(herbs) > 0:
+            index = random.randint(0, len(herbs) - 1)
+            herb = herbs.pop(index)
+            herb.eat()
+            new_herbs_list.append(herb)
 
+        # Do carnivore stuff
 
+        self.ini_pop = new_herbs_list
+
+    def grow(self):
+        self.island.set_fodder(self.highland_params['f_max'])
+
+    def age(self):
+        for animal in self.ini_pop:
+            animal.set_age(animal.get_age() + 1)
 
     def add_population(self, population):
         """
         Add a population to the island
         :param population: List of """
 
-    @property
-    def year(self):
-    """Last year simulated."""
-
-    @property
-    def num_animals(self):
-    """Total number of animals
-    dictionaries specifying population
-    on island."""
-
-    @property
-    def num_animals_per_species(self):
-    """Number of animals per species in island, as dictionary."""
-
-    def make_movie(self):
-    """Create MPEG4 movie from visualization images saved."""
+    # @property
+    # def year(self):
+    # """Last year simulated."""
+    #
+    # @property
+    # def num_animals(self):
+    # """Total number of animals
+    # dictionaries specifying population
+    # on island."""
+    #
+    # @property
+    # def num_animals_per_species(self):
+    # """Number of animals per species in island, as dictionary."""
+    #
+    # def make_movie(self):
+    # """Create MPEG4 movie from visualization images saved."""
--- biosim/animals.py
+++ biosim/animals.py
@@ -14,15 +14,17 @@ class Animals:
 
 
 class Herbs(Animals):
-    def __init__(self, age, w_birth, sigma_birth, phi_age, a_half, phi_weight, w_half):
+    def __init__(self, age, weight, cell, sigma_birth, phi_age, a_half, phi_weight, w_half, beta, F):
         super().__init__(age)
-        self.w_birth = w_birth
+        self.cell = cell
         self.sigma_birth = sigma_birth
         self.phi_age = phi_age
         self.a_half = a_half
         self.phi_weight = phi_weight
         self.w_half = w_half
-        self.weight = rand.gauss(self.w_birth, self.sigma_birth)
+        self.weight = weight
+        self.beta = beta
+        self.F = F
 
     def set_weight(self, weight):
         self.weight = weight
@@ -30,13 +32,24 @@ class Herbs(Animals):
     def get_weight(self):
         return self.weight
 
+    def eat(self):
+        food = self.cell.get_fodder()
+        if food > 0:
+            if food < self.F:
+                self.cell.set_fodder(0)
+                self.weight += (food * self.beta)
+            else:
+                self.cell.set_fodder(food - self.F)
+                self.weight += (self.F * self.beta)
+
     def get_fitness(self):
         if self.weight <= 0:
             return 0
         else:
             fitness = ((1 / (1 + exp(self.phi_age * (self.age - self.a_half)))) *
-             (1 / (1 + exp(self.phi_weight * (self.weight - self.w_half)))))
+                       (1 / (1 + exp(self.phi_weight * (self.weight - self.w_half)))))
             return fitness
 
     def is_dead(self):
+        if self.weight == 0:
             return self.get_weight() == 0
