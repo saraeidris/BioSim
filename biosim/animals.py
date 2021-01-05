5eb376789450d5257d7ea9514063c610f30f0907
saraidris <said@nmbu.no>
2021-01-05 11:50:37 +0100

    new animal class

---
+++ biosim/animals.py
@@ -0,0 +1,23 @@
+
+
+class Animals:
+    def __init__(self):
+
+
+class Herbs(Animals):
+    DEFAULT_PARAMETER = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9, 'eta': 0.05,
+                         'a_half': 40.0, 'phi_age': 0.6, 'w_half': 10.0,
+                         'phi_weight': 0.1, 'mu': 0.25, 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                         'omega': 0.4, 'F': 10.0}
+
+    def birth(self):
+
+    def age(self):
+
+    def weight(self):
+
+    def fitness(self):
+
+    #def migration(self):
+
+    def death(self):
