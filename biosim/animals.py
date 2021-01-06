a6ac2aded8c9a169ac6aef426e17e848a79f1dfe
thorbjornlundonsaker <thon@nmbu.no>
2021-01-06 20:39:24 +0100

    created test_animals, updated biosim and animals

--- biosim/animals.py
+++ biosim/animals.py
@@ -15,7 +15,7 @@ class Animals:
 
 class Herbs(Animals):
     def __init__(self, age, weight, cell, sigma_birth, phi_age, a_half, phi_weight, w_half, beta, F, gamma, zeta,
-                 w_birth):
+                 w_birth, omega):
         super().__init__(age)
         self.cell = cell
         self.sigma_birth = sigma_birth
@@ -29,14 +29,13 @@ class Herbs(Animals):
         self.gamma = gamma
         self.zeta = zeta
         self.w_birth = w_birth
+        self.omega = omega
 
     def set_weight(self, weight):
         self.weight = weight
 
     def get_weight(self):
         return self.weight
-
-
 
     def eat(self):
         food = self.cell.get_fodder()
@@ -57,14 +56,14 @@ class Herbs(Animals):
             return fitness
 
     def mate(self, ini_pop):
-        born_herbs = []
         if len(ini_pop) > 1:
             for _ in ini_pop:
                 if not self.weight < self.zeta(self.w_birth + self.sigma_birth):
-                    if self.gamma * self.get_fitness() * (len(ini_pop) - 1) < random.random():
+                    if random.random() < self.gamma * self.get_fitness() * (len(ini_pop) - 1):
+                        return True
+                    return False
+        else:
+            return False
 
-
-    # def is_dead(self):
-    #     if self.get_weight == 0:
-    #         self.ini_pop.pop()
-    #     else:
+    def dies(self):
+        return self.get_weight == 0 or random.random() < self.omega * (1 - self.get_fitness())
