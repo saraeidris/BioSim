629d9cce3dd6346dd00c19343110d848a7e91db6
saraidris <said@nmbu.no>
2021-01-08 16:41:56 +0100

    updated animals, BioSim, landscape

--- biosim/RossumIsland.py
+++ biosim/RossumIsland.py
@@ -5,10 +5,16 @@ class RossumIsland:
         self.ini_pop = ini_pop
         self.species = species
 
+    def make_island(self):
+        self.island_map.splitlines()
+
+
+
     def count_animals(self):
         num_animals = []
         for specie in self.species:
             num_animals.append(specie)
+
 
 
 
