b594d3e16c71d9919ef11aa8fef1d796f86f8316
saraidris <said@nmbu.no>
2021-01-17 11:32:28 +0100

    made some new docstrings and changed name from init_pop to insert population

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -26,21 +26,8 @@ class BioSim:
         :param cmax_animals: Dict specifying color-code limits for animal densities
         :param hist_specs: Specifications for histograms, see below
         :param img_base: String with beginning of file name for figures, including path
-        :param img_fmt: String with file type for figures, e.g. ’png’
-        If ymax_animals is None, the y-axis limit should be adjusted automatically.
-        If cmax_animals is None, sensible, fixed default values should be used.
-        cmax_animals is a dict mapping species names to numbers, e.g.,
-            {’Herbivore’: 50, ’Carnivore’: 20}
-        hist_specs is a dictionary with one entry per property for which a histogram shall be shown.
-        For each property, a dictionary providing the maximum value and the bin width must be
-        given, e.g.,
-            {’weight’: {’max’: 80, ’delta’: 2}, ’fitness’: {’max’: 1.0, ’delta’: 0.05}}
-        Permitted properties are ’weight’, ’age’, ’fitness’.
-        If img_base is None, no figures are written to file.
-        Filenames are formed as
-            ’{}_{:05d}.{}’.format(img_base, img_no, img_fmt)
-        where img_no are consecutive image numbers starting from 0.
-        img_base should contain a path and beginning of a file name. """
+        :param img_fmt: String with file type for figures, e.g. ’png’"""
+
         random.seed(seed)
         self.ymax_animals = ymax_animals
         self.cmax_animals = cmax_animals
@@ -49,7 +36,7 @@ class BioSim:
         self.img_fmt = img_fmt
         self.ini_pop = ini_pop
         self.island = RossumIsland(island_map)
-        self.island.set_init_population(self.ini_pop)
+        self.island.insert_population(self.ini_pop)
         self.island_map = island_map
         self.animal = 0
         self._step = 0
@@ -90,6 +77,12 @@ class BioSim:
 
     @staticmethod
     def merge_params(params1, params2):
+        """
+        merges default parameters with wanted parameters
+        :param params1: default parameters
+        :param params2: wanted parameters
+        :return:
+        """
         return {**params1, **params2}
 
     def set_landscape_parameters(self, landscape, params):
@@ -144,12 +137,12 @@ class BioSim:
         """
         Add a population to the island
         :param population: List of """
-        self.island.set_init_population(population)
+        self.island.insert_population(population)
 
     @property
     def year(self):
         """Last year simulated."""
-        return self._year
+        return self.year
 
     @property
     def num_animals(self):
