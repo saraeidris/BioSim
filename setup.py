4b80ac9a91a772c2bce6d0d6190d9b7557ca9728
saraidris <said@nmbu.no>
2021-01-19 20:58:20 +0100

    added setup file and MANIFEST

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='BioSim',
+      version='0.1',
+      description='A simulation of the ecosystem on Rossum Island.',
+      author='Sara E Idris, Thorbjørn L Onsaker, NMBU',
+      author_email='said@nmbu.no, thon@nmbu.no',
+      requires=['numpy', 'matplotlib'],
+      packages=['biosim'],
+      scripts=['examples/check_sim_disease.py', 'examples/herbivores_one_cell.py',
+               'examples/one_cell_both_species.py', 'examples/migration_test.py'])
