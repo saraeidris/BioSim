bc8f69535881ab2ccc5d9220a539fed8d1b78e33
saraidris <said@nmbu.no>
2021-01-20 09:57:29 +0100

    Added scipy and pytest to setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='BioSim',
       description='A simulation of the ecosystem on Rossum Island.',
       author='Sara E Idris, Thorbjørn L Onsaker, NMBU',
       author_email='said@nmbu.no, thon@nmbu.no',
-      requires=['numpy', 'matplotlib'],
+      requires=['numpy', 'matplotlib', 'scipy', 'pytest'],
       packages=['biosim'],
       scripts=['examples/check_sim_disease.py', 'examples/herbivores_one_cell.py',
                'examples/one_cell_both_species.py', 'examples/migration_test.py'])
